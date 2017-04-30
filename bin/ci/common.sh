3355dea102095dbee6c62b1690fcaee6e1539c7c
Ash Wilson <smashwilson@gmail.com>
2017-04-29 21:57:17 -0400

    Wait for the database to launch

--- bin/ci/common.sh
+++ bin/ci/common.sh
@@ -14,3 +14,20 @@ else
 fi
 
 export IMAGE_TAG="${IMAGE_BASE}:${BRANCH}"
+
+wait_for_postgres()
+{
+  TRIES=120
+  while [ ${TRIES} -gt 0 ]; do
+    printf '[%03d] Attempting PostgreSQL connection: ' ${TRIES}
+    if psql --quiet --command='SELECT 1;' --no-password "${DATABASE_URL}" ; then
+      printf "<hacker voice> I'm in\n"
+      return 0
+    fi
+
+    printf 'no\n'
+    sleep 0.5
+  done
+  printf 'Unable to connect to PostgreSQL.\n' >/dev/stderr
+  return 1
+}
