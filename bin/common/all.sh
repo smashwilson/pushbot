9e23fbe591fb0b600832a95eee797ec5fdc32831
Ash Wilson <smashwilson@gmail.com>
2017-06-03 21:27:06 -0400

    :fire: functionality that has been subsumed by the common scripts

--- bin/common/all.sh
+++ bin/common/all.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-source bin/common/image.sh
-source bin/common/wait.sh
+wait_for_postgres()
+{
+  TRIES=120
+  while [ ${TRIES} -gt 0 ]; do
+    printf '[%03d] Attempting PostgreSQL connection: ' ${TRIES}
+    if psql --quiet --command='SELECT 1;' --no-password "${DATABASE_URL}" >/dev/null 2>/dev/null ; then
+      printf "<hacker voice> I'm in\n"
+      return 0
+    fi
+
+    printf 'no\n'
+    sleep 0.5
+    (( TRIES-- ))
+  done
+  printf 'Unable to connect to PostgreSQL.\n' >/dev/stderr
+  return 1
+}
