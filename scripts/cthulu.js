b2be97263b5c7b516b5e119934cc2646e4f6e7bc
Ash Wilson <smashwilson@github.com>
2017-06-09 15:03:23 -0400

    Summon the Great Old One with JavaScript

---
+++ scripts/cthulu.js
@@ -0,0 +1,12 @@
+// Description:
+//   Summon a Great Old One
+// Commands:
+//   Ia! Ia! Cthulhu fhtagn! - Awaken the Great Old One
+
+module.exports = function (robot) {
+  robot.hear(/Ia! Ia! Cthulhu fhtagn!/i, msg => {
+    msg.send(
+      ':tentacle2: :tentacle2: :tentacle2: :tentacle2: :cthulhuul::cthulhuur: :tentacle: :tentacle: :tentacle: :tentacle:\n' +
+      ':tentacle2: :tentacle2: :tentacle2: :tentacle2: :cthulhull::cthulhulr: :tentacle: :tentacle: :tentacle: :tentacle:'
+   });
+}
