18c24a7246ac529a6a5a19d9c0006e52bedc91d3
Ash Wilson <smashwilson@gmail.com>
2017-11-04 18:18:27 -0400

    Testssss

--- test/mappings.test.js
+++ test/mappings.test.js
@@ -4,80 +4,104 @@ const helper = new Helper('../scripts/mapping.js')
 describe('mappings', function () {
   let room
 
-  beforeEach(function () {
+  beforeEach(async function () {
     room = helper.createRoom({httpd: false})
-
     room.robot.postgres = global.database
+    await loadAuth(room)
 
-    room.robot.auth = {
-      isAdmin: user => user.name === 'me',
-      hasRole: user => user.name === 'me'
-    }
+    const brain = room.robot.brain
+    brain.userForId('admin', {name: 'admin', roles: ['mapmaker']})
+    brain.userForId('fancy', {name: 'fancy', roles: ['fancy lad']})
+    brain.userForId('dandy', {name: 'dandy', roles: ['dandy lad']})
   })
 
-  afterEach(function () {
-    return room.user.say('me', '@hubot destroymapping foo')
-    .then(delay(200))
-    .then(() => room.destroy())
+  afterEach(async function () {
+    await room.user.say('admin', '@hubot destroymapping foo')
+    await delay(200)()
+    await room.destroy()
   })
 
   function responses () {
     return room.messages.filter(pair => pair[0] === 'hubot').map(pair => pair[1])
   }
 
-  function response (expected) {
-    return delay()().then(() => {
-      if (responses().indexOf(expected) === -1) {
-        console.log(room.messages)
-        expect.fail('', '', `"${expected}" response not seen.`)
-      }
-    })
+  async function response (expected) {
+    await delay()()
+    if (responses().indexOf(expected) === -1) {
+      console.log(room.messages)
+      throw new Error(`"${expected}" response not seen.`)
+    }
   }
 
-  it('creates a new mapping with !createmapping', function () {
+  it('creates a new mapping with !createmapping', async function () {
     usesDatabase(this)
 
-    return room.user.say('me', '@hubot createmapping foo')
-    .then(() => response('@me mapping foo has been created. :sparkles:'))
-    .then(delay(200))
-    .then(() => room.user.say('me', '@hubot setfoo @me: words words words'))
-    .then(() => response("me's foo has been set to 'words words words'."))
-    .then(() => room.user.say('me', '@hubot foo'))
-    .then(() => response('words words words'))
+    await room.user.say('admin', '@hubot createmapping foo')
+    await response('@admin mapping foo has been created. :sparkles:')
+    await delay(200)()
+    await room.user.say('admin', '@hubot setfoo @admin: words words words')
+    await response("admin's foo has been set to 'words words words'.")
+    await room.user.say('admin', '@hubot foo')
+    await response('words words words')
   })
 
-  it('specifies the null message with --null', function () {
+  it('specifies the null message with --null', async function () {
     usesDatabase(this)
 
-    return room.user.say('me', '@hubot createmapping foo --null="Nothing here."')
-    .then(() => response('@me mapping foo has been created. :sparkles:'))
-    .then(delay(500))
-    .then(() => room.user.say('me', '@hubot foo'))
-    .then(() => response('Nothing here.'))
+    await room.user.say('admin', '@hubot createmapping foo --null="Nothing here."')
+    await response('@admin mapping foo has been created. :sparkles:')
+    await delay(500)()
+    await room.user.say('admin', '@hubot foo')
+    await response('Nothing here.')
   })
 
-  it('fails if the mapping already exists', function () {
+  it('configures the role required to set your own with --role-own', async function () {
     usesDatabase(this)
 
-    return room.user.say('me', '@hubot createmapping foo')
-    .then(delay(200))
-    .then(() => room.user.say('me', '@hubot createmapping foo'))
-    .then(() => response("There's already a mapping called foo, silly!"))
+    await room.user.say('admin', '@hubot createmapping foo --role-own "fancy lad"')
+    await response('@admin mapping foo has been created. :sparkles:')
+    await delay(500)()
+
+    await room.user.say('dandy', '@hubot setfoo: nope')
+    await response("@dandy You can't do that! You're not a *fancy lad*.\n" +
+            'Ask an admin to run `hubot grant dandy the fancy lad role`.')
+
+    await room.user.say('fancy', '@hubot setfoo: yep')
+    await response("fancy's foo has been set to 'yep'.")
+
+    await room.user.say('dandy', '@hubot foo')
+    await response("I don't know any foos that contain that!")
+
+    await room.user.say('fancy', '@hubot foo')
+    await response('yep')
   })
 
-  it('destroys a mapping with !destroymapping', function () {
+  it('configures the role required to set others with --role-other')
+
+  it('configures the banner shown before !all with --all-banner')
+
+  it('fails if the mapping already exists', async function () {
     usesDatabase(this)
 
-    return room.user.say('me', '@hubot createmapping foo')
-    .then(delay(200))
-    .then(() => room.user.say('me', '@hubot destroymapping foo'))
-    .then(() => response('@me mapping foo has been destroyed. :fire:'))
+    await room.user.say('admin', '@hubot createmapping foo')
+    await delay(200)
+    await room.user.say('admin', '@hubot createmapping foo')
+    await response("There's already a mapping called foo, silly!")
   })
 
-  it('is okay to destroy a nonexistent mapping', function () {
+  it('destroys a mapping with !destroymapping', async function () {
     usesDatabase(this)
 
-    return room.user.say('me', '@hubot destroymapping blerp')
-    .then(() => response('@me mapping blerp does not exist.'))
+    await room.user.say('admin', '@hubot createmapping foo')
+    await delay(200)()
+    await room.user.say('admin', '@hubot destroymapping foo')
+    await response('@admin mapping foo has been destroyed. :fire:')
+  })
+
+  it('is okay to destroy a nonexistent mapping', async function () {
+    usesDatabase(this)
+
+    await room.user.say('admin', '@hubot destroymapping blerp')
+    await response('@admin mapping blerp does not exist.')
   })
 })
