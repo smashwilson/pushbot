9821f45fe7b25685d702b75dbbb4610ff9de6337
Ash Wilson <smashwilson@gmail.com>
2017-11-04 18:17:05 -0400

    Use yargs for command-line-ish parsing

--- scripts/helpers/index.js
+++ scripts/helpers/index.js
@@ -1,5 +1,7 @@
 // Grab-bag of utility functions.
 
+const yargs = require('yargs')
+
 function atRandom (list) {
   const max = list.length - 1
   const index = Math.floor(Math.random() * (max + 1))
@@ -27,7 +29,28 @@ function getDataStore (robot) {
   return dataStore
 }
 
+// Parse command options provided with shell-like syntax
+
+function parseArguments (msg, argline, fn) {
+  const y = fn(yargs)
+  return new Promise(resolve => {
+    y.parse(argline, (err, argv, output) => {
+      if (err) {
+        msg.reply(`:boom: You broke it!\n${msg}\n${this.yargs.help()}`)
+        resolve(null)
+      }
+
+      if (output) {
+        msg.reply(output)
+      }
+
+      resolve(argv)
+    })
+  })
+}
+
 module.exports = {
   atRandom,
-  getDataStore
+  getDataStore,
+  parseArguments
 }
