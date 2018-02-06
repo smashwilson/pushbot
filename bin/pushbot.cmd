5e9c2f55041941b68f4612cc6611a479b49aa1c6
Ash Wilson <smashwilson@gmail.com>
2018-02-05 21:20:42 -0500

    Try comparing unset variables another way?

--- bin/pushbot.cmd
+++ bin/pushbot.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-IF NOT DEFINED DEV_USERNAME (SET DEV_USERNAME=%USER%)
+IF "%DEV_USERNAME%" == "" (SET DEV_USERNAME=%USER%)
 set HUBOT_AUTH_ADMIN=1
 
 docker-compose run --service-ports pushbot
