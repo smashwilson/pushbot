305e750c9f42f8d13e3f42a078f655938117b0d5
Ash Wilson <smashwilson@gmail.com>
2018-03-18 10:42:29 -0400

    Expose an iCal feed to authenticated users

---
+++ scripts/models/calendar.js
@@ -0,0 +1,44 @@
+const uuid = require('uuid/v1')
+
+class CalendarMap {
+  static inRobot (robot) {
+    if (!this.instance || this.instance.robot !== robot) {
+      this.instance = new CalendarMap(robot)
+    }
+    return this.instance
+  }
+
+  constructor (robot) {
+    this.robot = robot
+    this.calendars = new Map(robot.brain.get('hubot-plan:ical') || [])
+  }
+
+  getCalendar (userId, userTz) {
+    let existing = this.calendars.get(userId)
+    if (!existing) {
+      const calendarId = uuid()
+      this.calendars.set(userId, {calendarId, userTz})
+      this.robot.brain.set('hubot-plan:ical', Array.from(this.calendars))
+      return calendarId
+    } else if (existing.userTz !== userTz) {
+      this.calendars.set(userId, {calendarId: existing.calendarId, userTz})
+      this.robot.brain.set('hubot-plan:ical', Array.from(this.calendars))
+      return existing.calendarId
+    } else {
+      return existing.calendarId
+    }
+  }
+
+  isValid (calendarId) {
+    for (const assignedId of this.calendars.values()) {
+      if (assignedId === calendarId) {
+        return true
+      }
+    }
+    return false
+  }
+}
+
+module.exports = {
+  CalendarMap
+}
