f1bf1c206c9b76ca21d5905977e161f91fb6bdf6
Ash Wilson <smashwilson@gmail.com>
2018-04-20 22:53:30 -0400

    Implement a custom emoji cache

---
+++ scripts/models/emoji.js
@@ -0,0 +1,56 @@
+// 1 hour
+const MAX_AGE = 60 * 60 * 1000
+
+class EmojiCache {
+  constructor (robot) {
+    this.robot = robot
+    this.refreshPromise = null
+    this.resolveRefreshPromise = () => {}
+  }
+
+  async get (emojiName) {
+    let payload = this.robot.brain.get('slack:emoji')
+
+    if (!payload) {
+      await this.refresh()
+      payload = this.robot.brain.get('slack:emoji')
+    }
+
+    if (payload && Date.now() - payload.age >= MAX_AGE) {
+      this.refresh()
+    }
+
+    if (!payload) {
+      payload = {emoji: {}}
+    }
+
+    return payload.emoji[emojiName] || null
+  }
+
+  async refresh () {
+    const client = this.robot.adapter.client
+    if (!client || !client.web || !client.web.emoji || !client.web.emoji.list) {
+      return
+    }
+
+    if (this.refreshPromise) {
+      await this.refreshPromise()
+      return
+    }
+
+    this.refreshPromise = new Promise(resolve => {
+      this.resolveRefreshPromise = resolve
+    })
+
+    const payload = await client.web.emoji.list()
+    payload.age = Date.now()
+    this.robot.brain.set('slack:emoji', payload)
+    this.resolveRefreshPromise()
+  }
+}
+
+function emojiCacheFor (robot) {
+  return new EmojiCache(robot)
+}
+
+module.exports = {emojiCacheFor}
