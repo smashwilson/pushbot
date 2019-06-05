501af0d2d2ddbe59c6bee54591cec3d2dec83a05
Ash Wilson <smashwilson@gmail.com>
2019-06-04 20:13:32 -0400

    Get the bot.ps1 script actually working :eyes:

--- bin/bot.ps1
+++ bin/bot.ps1
@@ -1,21 +1,22 @@
 param (
-  [string]$configuration = ""
+    [string]$configuration = ""
 )
 
 if (!$env:DEV_USERNAME) {
-  $env:DEV_USERNAME = $env:USERNAME
+    $env:DEV_USERNAME = $env:USERNAME
 }
 
 if (Test-Path -Path "secrets\botrc.ps1") {
-  $secrets = "secrets\botrc.ps1"
-} else {
-  $secrets = "secrets\botrc.example.ps1"
+    $secrets = "secrets\botrc.ps1"
+}
+else {
+    $secrets = "secrets\botrc.example.ps1"
 }
 
 if ($configuration -ne "") {
-  $secrets = "secrets\botrc.$configuration.ps1"
+    $secrets = "secrets\botrc.$configuration.ps1"
 }
 
 . $secrets
 
-docker-compose --service-ports pushbot
+docker-compose run --service-ports pushbot
