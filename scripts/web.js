11a73c25c8b1736c280dd6d25367004630938ca2
Ash Wilson <smashwilson@github.com>
2019-06-24 13:10:38 -0400

    Remove stonith stuff

--- scripts/web.js
+++ scripts/web.js
@@ -17,7 +17,6 @@ const root = require("./api/root");
 const {CalendarMap} = require("./models/calendar");
 
 const PORT = 8080;
-const MAGICAL_WEAK_SPOT_TOKEN = process.env.MAGICAL_WEAK_SPOT_TOKEN || "ni";
 const PRIOR_ADDRESSES = (process.env.PRIOR_ADDRESSES || "")
   .split(/,/)
   .filter(address => address.length > 0);
@@ -38,16 +37,11 @@ const CORS_OPTIONS = {
 class Status {
   constructor() {
     this.database = "pending";
-    this.stonith = "pending";
 
     this.killed = false;
 
     this.databasePromise = new Promise((resolve, reject) => {
       this.databaseCallbacks = {resolve, reject};
-    });
-
-    this.stonithPromise = new Promise((resolve, reject) => {
-      this.stonithCallbacks = {resolve, reject};
     });
   }
 
@@ -61,18 +55,8 @@ class Status {
     this.databaseCallbacks.reject();
   }
 
-  stonithReady() {
-    this.stonith = "ok";
-    this.stonithCallbacks.resolve();
-  }
-
-  stonithFailed() {
-    this.stonith = "failed";
-    this.stonithCallbacks.reject();
-  }
-
   onlinePromise() {
-    return Promise.all([this.databasePromise, this.stonithPromise]);
+    return Promise.all([this.databasePromise]);
   }
 
   kill() {
@@ -84,13 +68,12 @@ class Status {
 
     if (this.killed) {
       overall = "killed";
-    } else if (this.database === "ok" && this.stonith === "ok") {
+    } else if (this.database === "ok") {
       overall = "ok";
     }
 
     return {
       database: this.database,
-      stonith: this.stonith,
       status: overall,
     };
   }
@@ -292,19 +275,6 @@ module.exports = function(robot) {
     })
   );
 
-  // Stonith
-
-  let accepting = PRIOR_ADDRESSES.length === 0;
-
-  robot.receiveMiddleware((context, next, done) => {
-    if (!accepting) {
-      done();
-      return;
-    }
-
-    next(done);
-  });
-
   const status = new Status();
 
   status.onlinePromise().then(
@@ -337,67 +307,6 @@ module.exports = function(robot) {
     robot.on("database-up", databaseCheck);
   }
 
-  Promise.all(
-    PRIOR_ADDRESSES.map(
-      addr =>
-        new Promise(resolve => {
-          robot.logger.debug(`Terminating prior instance at ${addr}.`);
-          request(
-            {
-              url: `${PRIOR_SCHEME}://${addr}:${PRIOR_PORT}/magical-weak-spot`,
-              method: "DELETE",
-              agentOptions: {
-                checkServerIdentity(servername, cert) {
-                  const altNames = cert.subjectaltname;
-                  if (!altNames) {
-                    return new Error(
-                      `Invalid certificate for ${addr}: certificate contains not SANs`
-                    );
-                  }
-
-                  for (const name of altNames.split(", ")) {
-                    if (name === "DNS:api.pushbot.party") {
-                      return undefined;
-                    }
-                  }
-
-                  return new Error(
-                    `Invalid certificate for ${addr}: ${altNames}`
-                  );
-                },
-              },
-              headers: {"x-token": MAGICAL_WEAK_SPOT_TOKEN},
-              timeout: 30,
-            },
-            (error, response, body) => {
-              if (error) {
-                robot.logger.warning(
-                  `Error when trying to terminate pre-existing bot at ${addr}:\n${error}`
-                );
-              }
-
-              if (response && response.statusCode !== 200) {
-                robot.logger.warning(
-                  `Unable to terminate pre-existing bot at ${addr}:\n${body}`
-                );
-              }
-
-              resolve();
-            }
-          );
-        })
-    )
-  ).then(
-    () => {
-      robot.logger.debug("All prior instances terminated.");
-      status.stonithReady();
-    },
-    err => {
-      robot.logger.error(`Unable to terminate prior instances.\n${err}`);
-      status.stonithFailed();
-    }
-  );
-
   // iCal feeds
 
   app.get("/ical/:id", (req, res) => {
@@ -421,26 +330,6 @@ module.exports = function(robot) {
 
   app.get("/healthz", (req, res) => {
     res.json(status.summarize());
-  });
-
-  // These things are a god damned liability
-
-  app.delete("/magical-weak-spot", (req, res) => {
-    res.set("Content-Type", "text/plain");
-
-    if (req.headers["x-token"] !== MAGICAL_WEAK_SPOT_TOKEN) {
-      robot.logger.debug(
-        "Rejecting magical weak spot trigger with incorrect token."
-      );
-      res.status(403).send("no");
-      return;
-    }
-
-    accepting = false;
-    status.kill();
-
-    robot.logger.info("Magical weak spot triggered. Entering quiet mode.");
-    res.send("ok");
   });
 
   app.listen(PORT, () => {
