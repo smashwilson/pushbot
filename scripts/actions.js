d2a3c9133678a164550498dae29d91573b25f1c5
Ash Wilson <smashwilson@github.com>
2020-08-13 13:51:05 -0400

    format

--- scripts/actions.js
+++ scripts/actions.js
@@ -1,7 +1,7 @@
 // Description:
 //   Respond to "/me text text text" in Slack identically to "_text text text_"
 
-const {SlackTextMessage} = require('hubot-slack/src/message');
+const {SlackTextMessage} = require("hubot-slack/src/message");
 
 module.exports = function (robot) {
   const adapter = robot.adapter;
@@ -13,18 +13,32 @@ module.exports = function (robot) {
       const {user, channel} = event;
 
       if (event.type === "message" && event.subtype === "me_message") {
-        robot.logger.debug(`Received /me message in channel: ${channel}, from ${user.id} (human)`);
+        robot.logger.debug(
+          `Received /me message in channel: ${channel}, from ${user.id} (human)`
+        );
 
-        event.text = `_${event.text}_`
+        event.text = `_${event.text}_`;
 
-        SlackTextMessage.makeSlackTextMessage(user, undefined, undefined, event, channel, robot.name, robot.alias, client, (error, message) => {
-          if (error) {
-            robot.logger.error(`Dropping message due to error ${error.message}`);
-          } else {
-            adapter.receive(message);
+        SlackTextMessage.makeSlackTextMessage(
+          user,
+          undefined,
+          undefined,
+          event,
+          channel,
+          robot.name,
+          robot.alias,
+          client,
+          (error, message) => {
+            if (error) {
+              robot.logger.error(
+                `Dropping message due to error ${error.message}`
+              );
+            } else {
+              adapter.receive(message);
+            }
           }
-        })
+        );
       }
-    })
+    });
   }
-}
+};
